2988cac918c6fe2a3b12fd796cd49e94d2088b99
love19862003 <love19862003@163.com>
2019-08-26 14:18:15 +0800

    脚本更新-----------
    Signed-off-by: love19862003 <love19862003@163.com>

--- bin/pack.bat
+++ bin/pack.bat
@@ -1,2 +1,31 @@
-.\release\tool.exe .\setting.xlsx v1.0.0.1 1
+@echo off
+rem svn --version && echo svn found||goto ERROR_CONTEXT
+
+setlocal enabledelayedexpansion
+
+rem svn info ./ > tem.txt
+rem for /f"delims=" %%a in ('findstr "Revision:" tem.txt') do (set svnState=%%a)
+rem set version=%svnState:~10%
+
+set version=4096
+echo svn version : %version%
+
+.\debug\tool.exe .\setting.xlsx v1.0.0.%version% 1
+
+if errorlevel 1 goto ERROR_DATA
+echo pack excel success
+
 pause
+exit
+
+:ERROR_CONTEXT
+color 2f
+echo please install svn for command line
+pause 
+exit
+
+:ERROR_DATA
+color 2f
+echo pack failed, please check data
+pause 
+exit
